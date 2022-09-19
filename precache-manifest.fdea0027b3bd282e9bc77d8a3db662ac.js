a775bfadf15ef30a87bb35020ecdf89dc27ee2e5
fedora <fedora@google.com>
2022-09-19 22:44:05 +0900

    first commit

---
+++ precache-manifest.fdea0027b3bd282e9bc77d8a3db662ac.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "249bc5247687fef3e022c3e849407ffc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "163a28025822cb4fe204",
+    "url": "/static/js/2.39bb858c.chunk.js"
+  },
+  {
+    "revision": "df32743ed051aa784d347b8223c278a1",
+    "url": "/static/js/2.39bb858c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a0dea0fdefcdd3249dc5",
+    "url": "/static/js/main.57d74645.chunk.js"
+  },
+  {
+    "revision": "04c771c1cdb42c50fe11",
+    "url": "/static/js/runtime-main.7454f71f.js"
+  }
+]);

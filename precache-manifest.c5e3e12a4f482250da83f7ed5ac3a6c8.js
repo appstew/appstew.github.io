e352d036e9735b5d58cde3dcd0d554cae953a3c3
fedora <fedora@google.com>
2022-09-20 12:11:16 +0900

    md

---
+++ precache-manifest.c5e3e12a4f482250da83f7ed5ac3a6c8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "70ec32ee9d9187fd7c7245c38c862103",
+    "url": "/index.html"
+  },
+  {
+    "revision": "163a28025822cb4fe204",
+    "url": "/static/js/2.39bb858c.chunk.js"
+  },
+  {
+    "revision": "df32743ed051aa784d347b8223c278a1",
+    "url": "/static/js/2.39bb858c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7fa4ded6205c331be051",
+    "url": "/static/js/main.fbc7bcf9.chunk.js"
+  },
+  {
+    "revision": "04c771c1cdb42c50fe11",
+    "url": "/static/js/runtime-main.7454f71f.js"
+  }
+]);
